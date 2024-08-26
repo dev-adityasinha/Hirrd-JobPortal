1e1458fdee64faa4e41c62781ce560db1e05a724
codegeek-aditya <myidisadityasinha@gmail.com>
2024-08-26 19:36:34 +0530

    Ui Fixes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -78,7 +78,7 @@ const Header = () => {
 
       {showSignIn && (
         <div
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          className="fixed inset-0  flex items-center justify-center z-10 bg-opacity-95 bg-black"
           onClick={handleOverlayClick}
         >
           <SignIn
