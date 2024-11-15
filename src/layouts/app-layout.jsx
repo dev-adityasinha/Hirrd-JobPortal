90843ff61472af554753016554eac951f5d125b5
codegeek-aditya <myidisadityasinha@gmail.com>
2024-11-15 11:48:38 +0530

    Ui Fixes

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -9,9 +9,9 @@ const AppLayout = () => {
         <Header />
         <Outlet />
       </main>
-      {/* <div className="p-10 text-center bg-gray-800 mt-10">
+      <div className="p-10 text-center bg-gray-800 mt-10">
         Made with 💗 by Aditya Sinha
-      </div> */}
+      </div>
     </div>
   );
 };
